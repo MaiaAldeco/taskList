3d51be029179efc38121832fce6794004b4d8c18
MaiaAldeco <maiaaldeco@gmail.com>
2022-01-26 17:24:22 -0300

    Componente de lista de items

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Task} from '../../task'
 import {TASK} from '../../mock-tasks'
 
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
