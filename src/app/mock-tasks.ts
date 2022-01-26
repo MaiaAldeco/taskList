aaebbccb236e39e5df4a06bc98635c0af824d869
MaiaAldeco <maiaaldeco@gmail.com>
2022-01-26 16:47:02 -0300

    Agrego Mock de tareas e interface

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,32 @@
+import {Task} from "./task"
+
+export const TASK: Task[] = [
+    {
+        id: 1,
+        text: "Terminar módulo de Angular",
+        day: "Enero 30 a las 12:00",
+        reminder: true
+    },
+
+    {
+        id: 2,
+        text: "Investigar sobre Boostrap",
+        day: "Febrero 3 a las 16:30",
+        reminder: false
+    },
+
+    {
+        id: 3,
+        text: "Poner a prueba todo lo aprendido",
+        day: "Febrero 10 a las 14:00",
+        reminder: true
+    },
+
+    {
+        id: 4,
+        text: "Realizar el proyecto final",
+        day: "Marzo a las 09:00",
+        reminder: true
+    }
+
+]
